e6e1ff1b4ce55db729631ffe35c389e049e7ee53
Arthur Sena <arthur.silva.sena25@gmail.com>
2025-01-10 18:17:26 -0300

    criação da entity role

---
+++ src/main/java/com/sistemalogin/login/domain/entities/Role.java
@@ -0,0 +1,50 @@
+package com.sistemalogin.login.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Value{
+        ADMIN(1L),
+        BASIC(2L);
+
+        private Long valor;
+
+        Value(Long valor) {
+            this.valor = valor;
+        }
+
+        public Long getValor() {
+            return valor;
+        }
+
+        public void setValor(Long valor) {
+            this.valor = valor;
+        }
+    }
+}
